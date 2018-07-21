e6486017cfcc5c732333606ccbbc88bea64d6d10
Yamini Muralidharen <yaminivijaya@gmail.com>
2018-07-21 00:02:16 -0700

    payments api starter code

---
+++ starbucks-payments/src/main/java/com/sjsu/payments/StarbucksPaymentsApplication.java
@@ -0,0 +1,12 @@
+package com.sjsu.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarbucksPaymentsApplication.class, args);
+	}
+}
