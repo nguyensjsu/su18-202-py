60f1c05803b93b440af331f90a86ef9a083c95e8
Gyanesh Pandey <gyanesh.pandey@gmail.com>
2018-07-20 23:43:02 -0700

    Testing...

--- coffee-app/src/main/java/com/piecoffeeshop/App.java
+++ coffee-app/src/main/java/com/piecoffeeshop/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	AddCard ac = new AddCard("4123512361237123","999",20.0);
+        System.out.println( "AddCard: " + ac.getCardId() );
     }
 }
