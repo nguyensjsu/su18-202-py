9a7bcf310f7005828fce7cabecbcb81c0a988950
Abhi Yelipeddi <abhi.yelipeddi@sjsu.edu>
2018-07-21 11:14:55 -0700

    Initial commit for serverless order

---
+++ starbucks-orders/src/main/java/com/serverless/DeleteOrderHandler.java
@@ -0,0 +1,54 @@
+package com.serverless;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
+import java.util.Collections;
+import java.util.Map;
+
+import com.serverless.dal.Order;
+
+public class DeleteOrderHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+    private final Logger logger = Logger.getLogger(this.getClass());
+
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+
+    try {
+        // get the 'pathParameters' from input
+        Map<String,String> pathParameters =  (Map<String,String>)input.get("pathParameters");
+        String orderId = pathParameters.get("id");
+
+        // get the Order  by id
+        Boolean success = new Order().delete(orderId);
+
+        // send the response back
+        if (success) {
+          return ApiGatewayResponse.builder()
+                      .setStatusCode(204)
+                      .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                      .build();
+        } else {
+          return ApiGatewayResponse.builder()
+                      .setStatusCode(404)
+                      .setObjectBody("Order with id: '" + orderId + "' not found.")
+                      .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                      .build();
+        }
+    } catch (Exception ex) {
+        logger.error("Error in deleting order: " + ex);
+
+        // send the error response back
+              Response responseBody = new Response("Error in deleting order: ", input);
+              return ApiGatewayResponse.builder()
+                      .setStatusCode(500)
+                      .setObjectBody(responseBody)
+                      .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                      .build();
+    }
+    }
+}
