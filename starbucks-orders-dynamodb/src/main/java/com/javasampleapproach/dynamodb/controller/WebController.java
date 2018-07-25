3e0003b953309029315384cdf2419c542d2291cb
Abhi Yelipeddi <abhi.yelipeddi@sjsu.edu>
2018-07-24 23:20:28 -0700

    Added Size, Quantity attributes to the Order pojo Added a CostDecorator interface to set the options entered by user and to calculate the cost Added a Mocha class and if the user order is Mocha, this class is used to calculate the cost    based on the size and quantity ordered by the user Will be adding more classes of this type

--- starbucks-orders-dynamodb/src/main/java/com/javasampleapproach/dynamodb/controller/WebController.java
+++ starbucks-orders-dynamodb/src/main/java/com/javasampleapproach/dynamodb/controller/WebController.java
@@ -9,58 +9,64 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.javasampleapproach.dynamodb.model.Order;
+import com.javasampleapproach.dynamodb.model.*;
 import com.javasampleapproach.dynamodb.repo.OrderRepository;
 
 @RestController
 public class WebController {
-    
-    @Autowired
-    OrderRepository repository;
-    
-    @RequestMapping("/deleteall")
-    public String deleteall() {
-        repository.deleteAll();
-        return "All Orders Deleted";
-    }
-    
-    @RequestMapping(value = "/saveorder", method = RequestMethod.POST, produces = "application/json")
-    public String saveorder(@RequestBody Order order) {
-        // save a single Order
-        //repository.save(new Order("1", "Cappuchino", "2.50"));
-        
-        /* saving an order list */
-        //repository.save(Arrays.asList(new Order("2", "Latte", "3.50"), new Order("3", "Chai", "1.50")));
-        repository.save(order);
-        
-        return "Order Saved";
-    }
-    
-    @RequestMapping("/showall")
-    public String showAll() {
-        String result = "";
-        Iterable<Order> orders = repository.findAll();
-        
-        for (Order ord : orders) {
-            result += ord.toString() + "<br>";
-        }
-        
-        return result;
-    }
-    
-    @RequestMapping("/orderbyid")
-    public String orderById(@RequestParam("id") String id) {
-        String result = "";
-        result = repository.findOne(id).toString();
-        return result;
-    }
-    
-    @RequestMapping("/deletebyid")
-    public String deleteById(@RequestParam("id") String id) {
-        //String result = "";
-        repository.delete(id);
-        return "Deleted the Order";
-    }
-    
-    
+
+	@Autowired
+	OrderRepository repository;
+
+	@RequestMapping("/deleteall")
+	public String deleteall() {
+		repository.deleteAll();
+		return "All Orders Deleted";
+	}
+
+	@RequestMapping(value = "/saveorder", method = RequestMethod.POST, produces = "application/json")
+	public String saveorder(@RequestBody Order order) {
+		// save a single Order
+		//repository.save(new Order("1", "Cappuchino", "2.50"));
+
+		/* saving an order list */
+		//repository.save(Arrays.asList(new Order("2", "Latte", "3.50"), new Order("3", "Chai", "1.50")));
+		if (order.getOrderName().toString().equals("Mocha")) {
+			Mocha m = new Mocha();
+			String[] mo = { order.getSize().toString(), order.getQuantity().toString() };
+			m.setOptions(mo);
+			order.setCost(m.getCost().toString());
+		}
+		repository.save(order);
+
+		return "Order Saved";
+	}
+
+	@RequestMapping("/showall")
+	public String showAll() {
+		String result = "";
+		Iterable<Order> orders = repository.findAll();
+
+		for (Order ord : orders) {
+			result += ord.toString() + "<br>";
+		}
+
+		return result;
+	}
+
+	@RequestMapping("/orderbyid")
+	public String orderById(@RequestParam("id") String id) {
+		String result = "";
+		result = repository.findOne(id).toString();
+		return result;
+	}
+	
+	@RequestMapping("/deletebyid")
+	public String deleteById(@RequestParam("id") String id) {
+		//String result = "";
+		repository.delete(id);
+		return "Deleted the Order";
+	}
+
+	
 }
---
+++ starbucks-orders-dynamodb/src/main/java/com/javasampleapproach/dynamodb/model/CostDecorator.java
@@ -0,0 +1,6 @@
+package com.javasampleapproach.dynamodb.model;
+
+public interface CostDecorator {
+	Double getCost();
+	void setOptions(String[] options);
+}
