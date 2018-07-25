4b66f6c69956b8eea40889b651d2d69c05229033
Abhi Yelipeddi <abhi.yelipeddi@sjsu.edu>
2018-07-24 21:32:48 -0700

    Earlier I was hardcoding the add orders. With this fix, user can add orders using postman, curl etc

--- starbucks-orders-dynamodb/src/main/java/com/javasampleapproach/dynamodb/controller/WebController.java
+++ starbucks-orders-dynamodb/src/main/java/com/javasampleapproach/dynamodb/controller/WebController.java
@@ -3,7 +3,9 @@ package com.javasampleapproach.dynamodb.controller;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,52 +14,53 @@ import com.javasampleapproach.dynamodb.repo.OrderRepository;
 
 @RestController
 public class WebController {
-
-	@Autowired
-	OrderRepository repository;
-
-	@RequestMapping("/deleteall")
-	public String deleteall() {
-		repository.deleteAll();
-		return "All Orders Deleted";
-	}
-
-	@RequestMapping("/saveorder")
-	public String saveorder() {
-		// save a single Order
-		repository.save(new Order("1", "Cappuchino", "2.50"));
-
-		/* saving an order list */
-		repository.save(Arrays.asList(new Order("2", "Latte", "3.50"), new Order("3", "Chai", "1.50")));
-
-		return "Order Saved";
-	}
-
-	@RequestMapping("/showall")
-	public String showAll() {
-		String result = "";
-		Iterable<Order> orders = repository.findAll();
-
-		for (Order ord : orders) {
-			result += ord.toString() + "<br>";
-		}
-
-		return result;
-	}
-
-	@RequestMapping("/orderbyid")
-	public String orderById(@RequestParam("id") String id) {
-		String result = "";
-		result = repository.findOne(id).toString();
-		return result;
-	}
-	
-	@RequestMapping("/deletebyid")
-	public String deleteById(@RequestParam("id") String id) {
-		//String result = "";
-		repository.delete(id);
-		return "Deleted the Order";
-	}
-
-	
+    
+    @Autowired
+    OrderRepository repository;
+    
+    @RequestMapping("/deleteall")
+    public String deleteall() {
+        repository.deleteAll();
+        return "All Orders Deleted";
+    }
+    
+    @RequestMapping(value = "/saveorder", method = RequestMethod.POST, produces = "application/json")
+    public String saveorder(@RequestBody Order order) {
+        // save a single Order
+        //repository.save(new Order("1", "Cappuchino", "2.50"));
+        
+        /* saving an order list */
+        //repository.save(Arrays.asList(new Order("2", "Latte", "3.50"), new Order("3", "Chai", "1.50")));
+        repository.save(order);
+        
+        return "Order Saved";
+    }
+    
+    @RequestMapping("/showall")
+    public String showAll() {
+        String result = "";
+        Iterable<Order> orders = repository.findAll();
+        
+        for (Order ord : orders) {
+            result += ord.toString() + "<br>";
+        }
+        
+        return result;
+    }
+    
+    @RequestMapping("/orderbyid")
+    public String orderById(@RequestParam("id") String id) {
+        String result = "";
+        result = repository.findOne(id).toString();
+        return result;
+    }
+    
+    @RequestMapping("/deletebyid")
+    public String deleteById(@RequestParam("id") String id) {
+        //String result = "";
+        repository.delete(id);
+        return "Deleted the Order";
+    }
+    
+    
 }
